2250d5a4bed62c2ef21bdc39d7b0085342acc1eb
Renja-Nanzuu <ramadhananan001@gmail.com>
2026-03-11 01:45:29 +0700

    Hapus Folder Tugas4, ada yang salah

--- Tugas4/src/DemoBalok.java
+++
@@ -1,8 +0,0 @@
-public class DemoBalok {
-    public static void main(String[] args) {
-        Balok B = new Balok(2, 5, 10);
-        System.out.println("Operasi Hitung V dan LP Balok");
-        System.out.println("Volume Balok: " + B.VolumeBalok());
-        System.out.println("Luas Permukaan Balok: " + B.LPBalok());
-    }
-}
--- Tugas4/src/DemoKubus.java
+++
@@ -1,8 +0,0 @@
-public class DemoKubus {
-    public static void main(String[] args) {
-        Kubus K = new Kubus(7);
-        System.out.println("Operasi Hitung V dan LP Kubus");
-        System.out.println("Volume Balok: " + K.VolumeKubus());
-        System.out.println("Luas Permukaan Balok: " + K.LPKubus());
-    }
-}
